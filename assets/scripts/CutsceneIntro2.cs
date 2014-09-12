f6c0ee52472d3390bcf12d26b5b99c2fb2e18424
unknown <julio@drecon.com.br>
2014-09-12 14:21:43 -0300

    cutscene intro teste

--- assets/scripts/CutsceneIntro2.cs
+++ assets/scripts/CutsceneIntro2.cs
@@ -11,21 +11,38 @@ public class CutsceneIntro2 : Cutscene {
 	CutsceneIntro2.Action action = CutsceneIntro2.Action.Talk;
 	
 	void bfgTeleport(){
+		//Debug.Log ("Chegou!");
 		var tweenParams = new Hashtable();
-		tweenParams.Add("alpha", 0);
-		tweenParams.Add("time", 1);
-		iTween.FadeTo(car, tweenParams);
+		tweenParams.Add("x", 0);
+		tweenParams.Add("y", 0);
+		tweenParams.Add("z", 0);
+		tweenParams.Add("easetype", "easeInBounce");
+		iTween.ScaleTo(car, tweenParams);
 
 		var tweenParams2 = new Hashtable();
-		tweenParams2.Add("alpha", 0);
-		tweenParams2.Add("time", 1);
+		tweenParams2.Add("x", 0);
+		tweenParams2.Add("y", 0);
+		tweenParams2.Add("z", 0);
+		tweenParams2.Add("easetype", "easeInBounce");
 		tweenParams2.Add("oncomplete", "endCutscene");
 		tweenParams2.Add("oncompletetarget", gameObject);
-		iTween.FadeTo(bfg, tweenParams2);
+		iTween.ScaleTo(bfg, tweenParams2);
 	}
 	
 	void endCutscene(){
+		var tweenParams = new Hashtable();
+		tweenParams.Add("amount", 1);
+		tweenParams.Add("time", 2);
+		tweenParams.Add("oncompletetarget", gameObject);
+		tweenParams.Add("oncomplete", "playGame");
+
+		iTween.CameraFadeAdd();
+		iTween.CameraFadeTo(tweenParams);
 		stop ();
+	}
+
+	void playGame(){
+
 	}
 	
 	void Update () {
