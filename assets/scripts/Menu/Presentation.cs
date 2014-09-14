9492844f2f51f13d2991876cb428f0b7ac2fd810
stigmawall <wallace.wakko@gmail.com>
2014-09-14 12:46:14 -0300

    Apresentação

---
+++ assets/scripts/Menu/Presentation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Presentation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.FadeFrom( this.gameObject, 0, 1.4f );
+		Invoke ( "HidePresentation", 3 );
+	}
+	
+	void HidePresentation() 
+	{
+		iTween.FadeTo( this.gameObject, 0, 1.4f );
+		Invoke ( "GotoMenu", 1.4f );
+	}
+
+	void GotoMenu() {
+		Application.LoadLevel( "menu" );
+	}
+}
